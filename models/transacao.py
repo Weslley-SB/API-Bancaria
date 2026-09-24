fbd6484427cadad976125ad5a807952887f70c5f
Weslley <weslleysborges712@gmail.com>
2026-09-23 21:45:05 -0300

    feat: Adicionado alembic

--- models/transacao.py
+++ models/transacao.py
@@ -1,19 +1,18 @@
 import enum
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum, DateTime
 from database import Base
 import datetime
-
 class StatusTransacao(enum.Enum): #Opçoes do status das transações
     pendente = "pendente"
     completa = "completa"
     falha = "falha"
 
-class Transacoes(Base):
+class Transacao(Base):
     __tablename__ = "transacoes"
 
-    id = Column()
+    id = Column(Integer, primary_key=True, index=True)
     id_remetente = Column(Integer, ForeignKey("contas.id"), nullable=False, index=True)
     id_destinatario = Column(Integer, ForeignKey("contas.id"), nullable=False, index=True)
     valor = Column(Float, nullable=False)
-    data_hora = Column(String, default=lambda: datetime.datetime.now().isoformat())
+    data_hora = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
     status = Column(Enum(StatusTransacao), default=StatusTransacao.pendente, nullable=False)
